389af0085db1a0e5ffd27d658a4710c2f8bff65e
настя т 4110 <99817142+rsyomen@users.noreply.github.com>
2022-03-18 14:19:51 +0300

    Create hw2.cpp

---
+++ hw2.cpp
@@ -0,0 +1,20 @@
+2) но у меня не получается вывести все ли положительные 
+  #include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, pos=0, neg=0;
+    cout <<"введите числа" ;
+    
+    while (a!=0)
+    { cin >> a;
+    if (a>0) pos++;
+    else if (a<0) neg++;
+    else break;
+    }
+    if (a>0) cout <<"all positive" << endl; 
+    else cout <<"not all positive" << endl;
+    
+return 0;
+}
